883163ff3eb456033a529f7435af28d5887f926d
Max David <me@maxdavid.com>
2019-03-16 15:12:34 -0700

    Add recipe image fetching functionality
    Can now fetch random recipe pictures and fill Sketch image layers.
    
    TODO:
    Keyword input, instead of random
    Saved asset data for when API is not available
    Fill layers based on layer name

--- src/foodreau.js
+++ src/foodreau.js
@@ -1,16 +1,22 @@
 const sketch = require('sketch')
 const { DataSupplier, UI } = sketch
+const os = require('os')
+const path = require('path')
 const util = require('util')
+const fs = require('@skpm/fs')
+
 const api = require('./.secret.js')
 
 const API_ENDPOINT = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=1'
 const API_KEY = api.spoonacular.apikey
 const API_OPTIONS = { 'headers': { 'X-RapidAPI-Key': API_KEY } }
 
+const FOLDER = path.join(os.tmpdir(), 'com.sketchapp.foodreau-plugin')
 
 export function onStartup () {
   DataSupplier.registerDataSupplier('public.text', 'Random Recipe Title', 'SupplyRandomTitle')
   DataSupplier.registerDataSupplier('public.text', 'Random Recipe Source', 'SupplyRandomSource')
+  DataSupplier.registerDataSupplier('public.image', 'Random Recipe Image', 'SupplyRandomImage')
 }
 
 export function onShutdown () {
@@ -33,6 +39,14 @@ export function onSupplyRandomSource (context) {
   })
 }
 
+export function onSupplyRandomImage (context) {
+  const dataKey = context.data.key
+  const items = util.toArray(context.data.items).map(sketch.fromNative)
+  items.forEach((item, index) => {
+    let data = getRandomRecipeImage(item, index, dataKey)
+  })
+}
+
 export function getRandomRecipeSection (item, index, dataKey, section) {
   // section can be 'title' 'creditText' etc
   fetch(API_ENDPOINT, API_OPTIONS)
@@ -52,5 +66,60 @@ export function getRandomRecipeSection (item, index, dataKey, section) {
   function loadText (data, dataKey, index, item) {
     console.log(data)
     DataSupplier.supplyDataAtIndex(dataKey, data, index)
+  }
+}
+
+export function getRandomRecipeImage (item, index, dataKey) {
+  fetch(API_ENDPOINT, API_OPTIONS)
+    .then(res => res.json())
+    .then(json => {
+      if (json.message) {
+        return Promise.reject(json.message)
+      } else if (typeof json.recipes !== 'undefined') {
+        return json
+      } else {
+        return json
+      }
+    })
+    .then(json => loadImage(json.recipes[0]['image'], dataKey, index, item))
+    .catch(err => console.log(err))
+
+  function loadImage (data, dataKey, index, item) {
+    console.log(data)
+    return getImageFromURL(data).then(imagePath => {
+      if (!imagePath) {
+        // TODO: something wrong happened, show something to the user
+        return
+      }
+      DataSupplier.supplyDataAtIndex(dataKey, imagePath, index)
+    })
+  }
+
+  function getImageFromURL (url) {
+    return fetch(url)
+      .then(res => res.blob())
+      .then(saveTempFileFromImageData)
+      .catch((err) => {
+        console.error(err)
+        //return context.plugin.urlForResourceNamed('placeholder.png').path()
+      })
+  }
+
+  function saveTempFileFromImageData (imageData) {
+    const guid = NSProcessInfo.processInfo().globallyUniqueString()
+    const imagePath = path.join(FOLDER, `${guid}.jpg`)
+    try {
+      fs.mkdirSync(FOLDER)
+    } catch (err) {
+      // probably because the folder already exists
+      // TODO: check that it is really because it already exists
+    }
+    try {
+      fs.writeFileSync(imagePath, imageData, 'NSData')
+      return imagePath
+    } catch (err) {
+      console.error(err)
+      return undefined
+    }
   }
 }
