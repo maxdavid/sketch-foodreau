c18d0d4f45353e0c2dd3e393d7f9923338c1c7f4
Max David <me@maxdavid.com>
2019-04-01 13:28:37 -0700

    Working keyword search for text sections

--- src/foodreau.js
+++ src/foodreau.js
@@ -10,6 +10,8 @@ const FOLDER = path.join(os.tmpdir(), 'com.maxdavid.sketch.foodreau-plugin')
 const API_ENDPOINT = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/'
 const API_KEYS = ['title','creditText','sourceUrl','image','instructions','servings','readyInMinutes','extendedIngredients','vegetarian','vegan','glutenFree','dairyFree','veryHealthy','cheap','veryPopular','sustainable','lowFodmap','ketogenic','whole30','weightWatcherSmartPoints','pricePerServing','gaps','healthScore','id']
 const API_RANDOM_PARAM = 'random?number=1'
+const API_NUM_RESULTS = 10
+const API_QUERY_PARAM = 'search?number=' + API_NUM_RESULTS + '&query='
 
 let API_KEY = getAPIKey()
 let API_OPTIONS = { 'headers': { 'X-RapidAPI-Key': API_KEY } }
@@ -19,6 +21,7 @@ export function onStartup () {
   DataSupplier.registerDataSupplier('public.text', 'Random Recipe Title', 'SupplyRandomTitle')
   DataSupplier.registerDataSupplier('public.text', 'Random Recipe Source', 'SupplyRandomSource')
   DataSupplier.registerDataSupplier('public.text', 'Random Recipe Content from Layer Names', 'SupplyRandomContent')
+  DataSupplier.registerDataSupplier('public.text', 'Search Recipe...', 'SearchRecipe')
   DataSupplier.registerDataSupplier('public.image', 'Random Recipe Image', 'SupplyRandomImage')
 }
 
@@ -68,6 +71,130 @@ export function onSupplyRandomContent (context) {
       getRandomRecipeSection(item, index, dataKey, item.name)
     }
   })
+}
+
+export function onSearchRecipe (context) {
+  UI.getInputFromUser("Enter a recipe search term...",
+    { initialValue: 'burrito' },
+    (err, searchTerm) => { 
+      if (err) {
+        console.log(err)
+        return
+      } else {
+        const dataKey = context.data.key
+        const items = util.toArray(context.data.items).map(sketch.fromNative)
+        items.forEach((item, index) => {
+          if (!API_KEYS.includes(item.name) && item.type === 'Text') {
+            UI.message('"' + item.name + '" ' + 'is not a valid recipe field name')
+          } else {
+            getRecipe(item, index, dataKey, item.name, searchTerm)
+          }
+        })
+      }
+    }
+  )
+}
+
+function getRecipe (item, index, dataKey, section, searchTerm) {
+  // section must be in API_KEYS or item be an image
+  UI.message('Fetching recipe...')
+  if (item.type != 'Text') {
+    section = 'image'
+  }
+  let url = API_ENDPOINT
+  if (API_KEY) {
+    if (searchTerm) {
+      url += API_QUERY_PARAM + searchTerm
+    } else {
+      url += API_RANDOM_PARAM
+    }
+    fetch(url, API_OPTIONS)
+      .then(res => res.json())
+      .then(json => {
+        if (json.message) {
+          return Promise.reject(json.message)
+        } else if (typeof json.recipes !== 'undefined') {
+          loadData(json.recipes[0][section], dataKey, index, item)
+        } else if (typeof json.results !== 'undefined') {
+          console.log(json)
+          fetchRecipeInfo(json, dataKey, index, item, section, searchTerm)
+        } else {
+          return json
+        }
+      })
+      .catch(err => console.log(err))
+  } else {
+    loadData(BACKUP_RECIPES[section][Math.floor(Math.random() * BACKUP_RECIPES[section].length)], dataKey, index, item)
+  }
+
+  function fetchRecipeInfo (response) {
+    let url = API_ENDPOINT + response.results[0].id + '/information'
+    console.log(url)
+    fetch(url, API_OPTIONS)
+      .then(res => res.json())
+      .then(json => {
+        if (json.message) {
+          return Promise.reject(json.message)
+        } else {
+          console.log(json)
+          loadData(json[section], dataKey, index, item)
+          return json
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  function loadData (data, dataKey, index, item) {
+    //console.log(data)
+    if (item.type === 'Text') {
+      if (typeof data === 'object') {
+        data = convertIngredientsArray(data)
+      }
+      DataSupplier.supplyDataAtIndex(dataKey, data, index)
+    } else {
+      return getImageFromURL(data).then(imagePath => {
+        if (!imagePath) {
+          return
+        }
+        DataSupplier.supplyDataAtIndex(dataKey, imagePath, index)
+      })
+    }
+
+    function convertIngredientsArray (data) {
+      let text = ''
+      for (var ingredient in data) {
+        text += data[ingredient]['original'] + '\n'
+      }
+      return text
+    }
+
+    function getImageFromURL (url) {
+      return fetch(url)
+        .then(res => res.blob())
+        .then(saveTempFileFromImageData)
+        .catch((err) => {
+          console.error(err)
+          return context.plugin.urlForResourceNamed('placeholder.png').path()
+        })
+    }
+
+    function saveTempFileFromImageData (imageData) {
+      const guid = NSProcessInfo.processInfo().globallyUniqueString()
+      const imagePath = path.join(FOLDER, `${guid}.jpg`)
+      try {
+        fs.mkdirSync(FOLDER)
+      } catch (err) {
+        // probably because the folder already exists
+      }
+      try {
+        fs.writeFileSync(imagePath, imageData, 'NSData')
+        return imagePath
+      } catch (err) {
+        console.error(err)
+        return undefined
+      }
+    }
+  }
 }
 
 function getRandomRecipeSection (item, index, dataKey, section) {
